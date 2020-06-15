dc7c566a94ab91029b117fa6bdafd5a9fc62b883
Randall Flagg <russellfannin0@gmail.com>
2020-06-15 02:50:33 -0700

    Add fix beer cookbook recipe

---
+++ patches/2020-06-15 - trophy vendor/20200615015700-fixbeercookbook.sql
@@ -0,0 +1,9 @@
+INSERT INTO cook_book (recipe_id, source_w_c_i_d, target_w_c_i_d) values 
+(6859, 29180, 1910049),
+(6937, 29180, 1910054),
+(6866, 29180, 1910051),
+(6938, 29180, 1910055),
+(6939, 29180, 1910056),
+(6860, 29180, 1910052);
+
+#run /clearcache recipe
