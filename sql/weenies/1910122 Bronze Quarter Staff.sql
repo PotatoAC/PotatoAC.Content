7fe22f73e0dcaefb88eae7d94ef209d8ef426d05
Randall Flagg <russellfannin0@gmail.com>
2020-06-17 00:12:54 -0700

    fix quarter staff weenie

--- sql/weenies/1910122 Bronze Quarter Staff.sql
+++ sql/weenies/1910122 Bronze Quarter Staff.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 1910122;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (1910122, 'quarterstaffstatuebronze', 6, '2005-02-09 10:00:00') /* MeleeWeapon */;
+VALUES (1910122, 'ace1910122-quarterstaffstatuebronze', 6, '2005-02-09 10:00:00') /* MeleeWeapon */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (1910122,   1,          1) /* ItemType - MeleeWeapon */
