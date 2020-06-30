9b1d1e2d03e315814511dfad285942c57cea4c9c
Randall Flagg <russellfannin0@gmail.com>
2020-06-30 01:28:05 -0700

    Update Generator for DI

--- sql/weenies/Areas/DI/1910201 DI Gen.sql
+++ sql/weenies/Areas/DI/1910201 DI Gen.sql
@@ -4,8 +4,8 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (1910201, '1910201-DIGENERATOR', 1, '2005-02-09 10:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (1910201,  81,          18) /* MaxGeneratedObjects max total overrides below*/
-     , (1910201,  82,          18) /* InitGeneratedObjects spawned when placed overrides below*/
+VALUES (1910201,  81,          5) /* MaxGeneratedObjects max total overrides below*/
+     , (1910201,  82,          5) /* InitGeneratedObjects spawned when placed overrides below*/
      , (1910201,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -25,20 +25,4 @@ VALUES (1910201,   1,   33555051) /* Setup */
      , (1910201,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+VALUES (1910201, -1, 1910224/* weenie */, 600 /* respawn time */, 5 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0);
--- sql/weenies/Areas/DI/1910224 DI Gen One.sql
+++ sql/weenies/Areas/DI/1910224 DI Gen One.sql
@@ -25,24 +25,30 @@ VALUES (1910224,   1,   33555051) /* Setup */
      , (1910224,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (1910224, 0.009345, 1910203/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.0123, 1910204/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01625, 1910205/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.012345, 1910206/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01234, 1910207/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01365, 1910208/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01235, 1910209/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.0135, 1910210/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01655, 1910211/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01356, 1910212/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01525, 1910213/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.0164, 1910214/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.0134, 1910215/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01356, 1910216/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01256, 1910217/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.012465, 1910218/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01235, 1910219/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01235, 1910220/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01246, 1910221/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.012345, 1910222/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
-     , (1910224, 0.01236, 1910223/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+VALUES (1910224, 0.009345, 
+1910203/* weenie */,
+ 60 /* respawn time */, 
+ 1 /* spawn amount */,
+5 /*max spawn */, 
+1 /* spawn when 1 dies */, 
+2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.05, 1910204/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.1, 1910205/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.15, 1910206/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.2, 1910207/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.25, 1910208/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.3, 1910209/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.35, 1910210/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.4, 1910211/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.45, 1910212/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.5, 1910213/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.55, 1910214/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.6, 1910215/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.65, 1910216/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.7, 1910217/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.75, 1910218/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.8, 1910219/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.85, 1910220/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.9, 1910221/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 0.95, 1910222/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0)
+     , (1910224, 1.0, 1910223/* weenie */, 60 /* respawn time */, 1 /* spawn amount */, 5 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0);
