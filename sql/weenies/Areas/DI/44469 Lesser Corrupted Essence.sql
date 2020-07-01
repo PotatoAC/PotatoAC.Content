737e127aa1ea1628363b38c4eadbb9775aa75421
magemasterII <66111204+magemasterII@users.noreply.github.com>
2020-06-30 17:19:07 -0700

    Stackable Essences #23

--- sql/weenies/Areas/DI/44469 Lesser Corrupted Essence.sql
+++ sql/weenies/Areas/DI/44469 Lesser Corrupted Essence.sql
@@ -1,11 +1,15 @@
 DELETE FROM `weenie` WHERE `class_Id` = 44469;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (44469, 'ace44469-lessercorruptedessence', 1, '2020-06-18 08:44:49') /* Generic */;
+VALUES (44469, 'ace44469-lessercorruptedessence', 51, '2020-06-18 08:44:49') /* Stackable */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (44469,   1,        128) /* ItemType - Misc */
      , (44469,   5,        100) /* EncumbranceVal */
+     , (44469,  11,        100) /* MaxStackSize */
+     , (44469,  12,          1) /* StackSize */
+     , (44469,  13,          100) /* StackUnitEncumbrance */
+     , (44469,  15,          0) /* StackUnitValue */
      , (44469,  16,          1) /* ItemUseable - No */
      , (44469,  18,        128) /* UiEffects - Frost */
      , (44469,  19,          0) /* Value */
