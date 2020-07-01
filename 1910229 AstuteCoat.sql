8aff468e8dcf9eb6cbb91ed1fcb4501fcb266a49
FlaggAC <65846074+FlaggAC@users.noreply.github.com>
2020-06-30 20:02:22 -0700

    Update 1910229 AstuteCoat.sql

--- 1910229 AstuteCoat.sql
+++ 1910229 AstuteCoat.sql
@@ -1,7 +1,7 @@
 DELETE FROM `weenie` WHERE `class_Id` = 1910229;
 
 INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
-VALUES (1910229, 'astutecoat', 2, '2019-11-05 00:00:00') /* Clothing */;
+VALUES (1910229, 'astutecoatFOOBAR', 2, '2019-11-05 00:00:00') /* Clothing */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (1910229,   1,          2) /* ItemType - Armor */
