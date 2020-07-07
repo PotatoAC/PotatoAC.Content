d0a0876dc60643be30bca5f7a62317ead84ee8ef
magemasterII <66111204+magemasterII@users.noreply.github.com>
2020-07-07 09:42:19 -0700

    Update to ring announcement
    Ring announcement now says it just spawned somewhere rather than specifying DI coords so the generator can be placed anywhere

--- sql/weenies/Areas/DI/1910274 A Ring.sql
+++ sql/weenies/Areas/DI/1910274 A Ring.sql
@@ -210,7 +210,7 @@ VALUES (1910274, 9 /* Generation */,      1, NULL, NULL, NULL, NULL, NULL, NULL,
 SET @parent_id = LAST_INSERT_ID();
 
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (@parent_id,  0,   16 /* World broadcast */, 1, 1, NULL, 'A Ring has appeared in an ancient temple on an island filled with death.(92.2N, 55.1E)', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+VALUES (@parent_id,  0,   16 /* World broadcast */, 1, 1, NULL, 'A Ring of great power has appeared somewhere in the world.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 INSERT INTO `weenie_properties_emote` (`object_Id`, `category`, `probability`, `weenie_Class_Id`, `style`, `substyle`, `quest`, `vendor_Type`, `min_Health`, `max_Health`)
 VALUES (1910274,  10 /* Pickup */,   1, 1910274, NULL, NULL, NULL, NULL, NULL, NULL);
