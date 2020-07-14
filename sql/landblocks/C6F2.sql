facd790d98f33da5e983c4f874db0f09967389ce
Randall Flagg <russellfannin0@gmail.com>
2020-07-13 21:35:17 -0700

    Add Sword of Damocles and blinding crystal spawn to DI (fixed location)

---
+++ sql/landblocks/C6F2.sql
@@ -0,0 +1,29 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xC6F2;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2000, 1910201, 0xC6F2003D, 169.399, 116.416, 0, 0.993703, 0, 0, 0.112049, False, '2020-06-30 04:48:23'); /* DI Generator */
+/* @teleloc 0xC6F2003D [169.399002 116.416000 0.000000] 0.993703 0.000000 0.000000 0.112049 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2001, 1910316, 0xC6F2000B, 30.7083, 53.9415, 54.055, 0.992714, 0, 0, -0.120494, False, '2020-07-14 00:30:35');
+/* @teleloc 0xC6F2000B [30.708300 53.941502 54.055000] 0.992714 0.000000 0.000000 -0.120494 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2002, 1910316, 0xC6F2000B, 33.0303, 53.7775, 54.055, 0.999573, 0, 0, 0.0292088, False, '2020-07-14 00:30:37');
+/* @teleloc 0xC6F2000B [33.030300 53.777500 54.055000] 0.999573 0.000000 0.000000 0.029209 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2003, 1910316, 0xC6F2000B, 35.4366, 53.9183, 54.055, 0.999573, 0, 0, 0.0292088, False, '2020-07-14 00:30:38');
+/* @teleloc 0xC6F2000B [35.436600 53.918301 54.055000] 0.999573 0.000000 0.000000 0.029209 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2004, 1910316, 0xC6F2000B, 32.9754, 56.9478, 54.055, 0.999527, 0, 0, -0.0307439, False, '2020-07-14 00:30:41');
+/* @teleloc 0xC6F2000B [32.975399 56.947800 54.055000] 0.999527 0.000000 0.000000 -0.030744 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2005, 1910316, 0xC6F2000A, 33.0376, 46.2083, 54.055, 0.999527, 0, 0, -0.0307439, False, '2020-07-14 00:30:45');
+/* @teleloc 0xC6F2000A [33.037601 46.208302 54.055000] 0.999527 0.000000 0.000000 -0.030744 */
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7C6F2006, 1910314, 0xC6F2000B, 33.4241, 50.238, 54.055, -0.997943, 0, 0, 0.0641122, False, '2020-07-14 00:31:00'); /* Sword of Damocles generator */
+/* @teleloc 0xC6F2000B [33.424099 50.237999 54.055000] -0.997943 0.000000 0.000000 0.064112 */
--- sql/weenies/PvPWeapons/1910314 Sword of Damocles Generator.sql
+++ sql/weenies/PvPWeapons/1910314 Sword of Damocles Generator.sql
@@ -4,8 +4,8 @@ INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
 VALUES (1910314, '1910314', 1, '2005-02-09 10:00:00') /* Generic */;
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
-VALUES (1910314,  81,          1) /* MaxGeneratedObjects max total overrides below*/
-     , (1910314,  82,          1) /* InitGeneratedObjects spawned when placed overrides below*/
+VALUES (1910314,  81,          1) /* MaxGeneratedObjects */
+     , (1910314,  82,          1) /* InitGeneratedObjects */
      , (1910314,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
@@ -14,8 +14,8 @@ VALUES (1910314,   1, True ) /* Stuck */
      , (1910314,  18, True ) /* Visibility */;
 
 INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
-VALUES (1910314,  41,      10800) /* RegenerationInterval respawn timer doesnt overide below?*/
-     , (1910314,  43,      1) /* GeneratorRadius max 100 over 100 creates square spawn*/;
+VALUES (1910314,  41,   10800) /* RegenerationInterval */
+     , (1910314,  43,       1) /* GeneratorRadius */;
 
 INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
 VALUES (1910314,   1, 'Sword of Damocles generator') /* Name */;
@@ -25,4 +25,4 @@ VALUES (1910314,   1,   33555051) /* Setup */
      , (1910314,   8,  100667494) /* Icon */;
 
 INSERT INTO `weenie_properties_generator` (`object_Id`, `probability`, `weenie_Class_Id`, `delay`, `init_Create`, `max_Create`, `when_Create`, `where_Create`, `stack_Size`, `palette_Id`, `shade`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
-VALUES (1910314, -1, 1910309/* weenie */, 1 /* respawn time */, 1 /* spawn amount */, 1 /*max spawn */, 1 /* spawn when 1 dies */, 2 /* changes scatter type? */, -1, 0 /* color */, 0, 0, 0, 0, 0, 1, 0, 0, 0);
+VALUES (1910314, -1, 1910309, 1, 1, 1, 1, 2, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0) /* Generate Sword of Damocles (1910309) (x1 up to max of 1) - Regenerate upon Destruction - Location to (re)Generate: Scatter */;
