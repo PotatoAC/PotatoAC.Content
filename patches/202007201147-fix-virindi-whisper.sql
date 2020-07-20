642a63024746b8ae29277e854807bff6280b5e6b
Randall Flagg <russellfannin0@gmail.com>
2020-07-20 11:55:00 -0700

    several DB patches

---
+++ patches/202007201147-fix-virindi-whisper.sql
@@ -0,0 +1,14 @@
+update ace_world.spell set stat_mod_key = 14, stat_mod_val = 8
+where id = 5157;
+
+update ace_world.spell set stat_mod_key = 14, stat_mod_val = 16
+where id = 5158;
+
+update ace_world.spell set stat_mod_key = 14, stat_mod_val = 24
+where id = 5159;
+
+update ace_world.spell set stat_mod_key = 14, stat_mod_val = 32
+where id = 5155;
+
+update ace_world.spell set stat_mod_key = 14, stat_mod_val = 40
+where id = 5156;
